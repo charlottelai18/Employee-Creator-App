c109d2c13ae5f7f9290c933e124d5e0527e2861e
Charlotte Lai <z5413888@student.unsw.edu.au>
2026-02-26 23:47:09 +1100

    added employee api for frontend

---
+++ frontend/src/api/employeeApi.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+import type { Employee } from "../types";
+
+const API_URL = "http://localhost:8080/api/employees";
+
+export const getAllEmployees = async (): Promise<Employee[]> => {
+    const response = await axios.get<Employee[]>(API_URL);
+    return response.data;
+}
+
+export const getEmployeeById = async (id: number): Promise<Employee> => {
+    const response = await axios.get<Employee>(`${API_URL}/${id}`);
+    return response.data;
+}
+
+export const createEmployee = async (employee: Omit<Employee, 'id'>): Promise<Employee> => {
+    const response = await axios.post<Employee>(API_URL, employee);
+    return response.data;
+}
+
+export const updateEmployee = async (id: number, employee: Partial<Employee>): Promise<Employee> => {
+    const response = await axios.put<Employee>(`${API_URL}/${id}`, employee);
+    return response.data;
+}
+
+export const deleteEmployee = async (id: number): Promise<void> => {
+    await axios.delete(`${API_URL}/${id}`);
+}
