4760b714587c8a213da37626b1924876009667c2
Charlotte Lai <z5413888@student.unsw.edu.au>
2026-02-26 18:02:41 +1100

    added Employee Repository

---
+++ backend/src/main/java/com/employees/api/repository/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package com.employees.api.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.employees.api.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Optional<Employee> findByEmail(String email);
+}
