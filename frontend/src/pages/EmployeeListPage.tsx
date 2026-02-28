b1ac77d769e3190c977184cffaad710f417c3d45
Charlotte Lai <z5413888@student.unsw.edu.au>
2026-02-28 19:45:48 +1100

    extract employeeCard component for reusability

--- frontend/src/pages/EmployeeListPage.tsx
+++ frontend/src/pages/EmployeeListPage.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { getAllEmployees, deleteEmployee } from '../api/employeeApi'
 import type { Employee } from '../types'
+import EmployeeCard from '../components/EmployeeCard'
 
 const EmployeeListPage = () => {
     const [employees, setEmployees] = useState<Employee[]>([])
@@ -10,33 +11,56 @@ const EmployeeListPage = () => {
     const navigate = useNavigate()
 
     useEffect(() => {
-        const fetchEmployees = async() => {
+        const fetchEmployees = async () => {
             try {
                 setLoading(true)
-                const data = await getAllEmployees();
-                setEmployees(data);
-            } catch (err){
-                setError("Failed to Fetch Employees");
+                const data = await getAllEmployees()
+                setEmployees(data)
+            } catch (err) {
+                setError("Failed to fetch employees")
             } finally {
-                setLoading(false);
+                setLoading(false)
             }
         }
-        fetchEmployees();
+        fetchEmployees()
     }, [])
 
-    // handle delete
     const handleDelete = async (id: number) => {
         try {
             setLoading(true)
-            await deleteEmployee(id);
+            await deleteEmployee(id)
             setEmployees(employees.filter(emp => emp.id !== id))
-        } catch (err){
-            setError("Failed to delete Employee");
-        } finally{
-            setLoading(false);
+        } catch (err) {
+            setError("Failed to delete employee")
+        } finally {
+            setLoading(false)
         }
-    
     }
+
+    return (
+        <div>
+            <h1>Employees List</h1>
+
+            <button onClick={() => navigate('/employees/new')}>
+                Add Employee
+            </button>
+
+            {loading && <p>Loading...</p>}
+            {error && <p>{error}</p>}
+
+            {employees.map(employee => (
+                <EmployeeCard
+                    key={employee.id}
+                    employee={employee}
+                    onDelete={handleDelete}
+                />
+            ))}
+
+            {employees.length === 0 && !loading && (
+                <p>No employees found. Add one!</p>
+            )}
+        </div>
+    )
 }
 
 export default EmployeeListPage
